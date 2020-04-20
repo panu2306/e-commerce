75ef9ff4e48b783a48a2e0442cccc5f41f2ac7f7
panu2306 <pranavbhendawade@gmail.com>
2020-04-20 18:54:36 +0530

    e-commerce site project initialization

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/panu/Documents/profiles/work/django-projects/e-commerce/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
