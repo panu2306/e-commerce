7a973e8e2457fb02952d1faf08db6528a66dfd38
panu2306 <pranavbhendawade@gmail.com>
2020-04-21 00:21:20 +0530

    login page created

--- ecommerce/ecommerce/forms.py
+++ ecommerce/ecommerce/forms.py
@@ -25,3 +25,23 @@ class ContactForm(forms.Form):
                                           }
                                 )
                             )
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', 
+                               widget=forms.TextInput(
+                                   attrs={
+                                            "class":"form-control",
+                                            "placeholder":"Your Username"
+                                          }
+                                )
+                            )
+    password = forms.CharField(label='Password', 
+                               widget=forms.PasswordInput(
+                                   attrs={
+                                            "class":"form-control",
+                                            "placeholder":"Your Password"
+                                          }
+                                )
+                            )
+   
--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import forms
 
@@ -29,3 +30,40 @@ def contact_page(request):
         print('Email', request.POST.get('email'))
         print('Content', request.POST.get('content'))
     return render(request, 'contact_page.html', context=context)
+
+def login_page(request):
+    # login_form = forms.LoginForm(request.POST or None)
+    # context = {
+    #     'form': login_form
+    #     }
+    # print("User logged in")
+    # if(login_form.is_valid()):
+    #     print(login_form.cleaned_data)
+    #     username = login_form.cleaned_data.get('username')
+    #     password = login_form.cleaned_data.get('password')
+    #     user = authenticate(request, username=username, password=password)
+    #     print(user)
+
+    #     if(user is not None):
+    #         login(request, user)
+
+    #         return redirect('/login')
+    #     else:
+    #         print('ERROR')
+    # return render(request, 'auth/login.html', context=context)
+    if(request.method == 'POST'):
+        form = forms.LoginForm()
+        if(form.is_valid()):
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if(user is not None):
+                login(request, user)
+                return redirect('/login')
+            else:
+                print('Error')
+        else:
+            print('Form is invalid')
+    else:
+        form = forms.LoginForm()
+    return render(request, 'auth/login.html', context={'form':form})
