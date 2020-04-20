0d6e341d7adfd0ed9580941c8d5eda4ae81fce66
panu2306 <pranavbhendawade@gmail.com>
2020-04-20 23:05:45 +0530

    created about, home and contact page

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home_page'),
+    path('about/', views.about_page, name='about_page'),
+    path('contact/', views.contact_page, name='contact_page'),
 ]
--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -2,4 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse('Hello World!!!')
+    context = {
+        'title': 'Home Page',
+        'content':'Hello, Welcome to the home page'
+    }
+    return render(request, 'home_page.html', context=context)
+
+def about_page(request):
+    context = {
+        'title': 'About Page',
+        'content': 'Hello, Welcome to the about page'
+    }
+    return render(request, 'about_page.html', context=context)
+
+def contact_page(request):
+    context = {
+        'title': 'Contact Page',
+        'content': 'Hello, Welcome to the contact page'
+    }
+    return render(request, 'contact_page.html', context=context)
