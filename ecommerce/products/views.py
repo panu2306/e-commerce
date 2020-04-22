9763626d3cec62886af5b5fb1ff5f45111dc444b
panu2306 <pranavbhendawade@gmail.com>
2020-04-22 19:29:38 +0530

    product list view is created

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+
+def product_list(request):
+    qs = Product.objects.all()
+    context = {
+        'qs': qs
+    }
+    return render(request, 'products/product_list.html', context=context)
