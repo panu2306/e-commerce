d4aa70127d3b2895e5fdab304bf51a295d2625b3
panu2306 <pranavbhendawade@gmail.com>
2020-04-22 16:29:52 +0530

    products is added to admin

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,3 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return self.title
+    
